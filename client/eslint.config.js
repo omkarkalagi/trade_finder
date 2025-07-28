2cc5f92ec9260ceab7706850fe5c17fbb9baa1db
omkarkalagi <omkardigambar4@gmail.com>
2025-07-29 03:45:53 +0530

    Complete TypeScript migration foundation

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,58 +2,50 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
 
 export default [
-  js.configs.recommended,
+  // JavaScript configuration
   {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: './tsconfig.json'
-      }
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }
-      ]
-    }
-  },
-  {
+    files: ['**/*.js', '**/*.jsx'],
+    ...js.configs.recommended,
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+  // TypeScript configuration
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react': reactPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
       parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
     },
     rules: {
-      'no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }
-      ],
+      ...tsPlugin.configs['eslint-recommended'].rules,
+      ...tsPlugin.configs['recommended'].rules,
+      'no-unused-vars': 'off', // Turn off the base rule and use the TypeScript rule
+      '@typescript-eslint/no-unused-vars': 'warn',
       'no-console': 'off',
     },
-    ignores: ['dist/**', 'node_modules/**'],
   },
 ];
