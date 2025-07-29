170bb1e5beafbcbd84400641369bf6c8022b05fe
omkarkalagi <omkardigambar4@gmail.com>
2025-07-29 07:45:42 +0530

    Properly configure @tailwindcss/postcss

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,6 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {
+      tailwindConfig: './tailwind.config.js'
+    },
     autoprefixer: {},
   }
 };
