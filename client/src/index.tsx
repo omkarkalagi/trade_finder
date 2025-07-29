b59466716fc044c6c38338f091ec5772736f8126
omkarkalagi <omkardigambar4@gmail.com>
2025-07-29 06:54:46 +0530

    Fix syntax error in index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
