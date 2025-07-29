1728c4bbca43664d38597e952be49f9f44e60e45
omkarkalagi <omkardigambar4@gmail.com>
2025-07-29 07:47:29 +0530

    Fix @tailwindcss/postcss installation

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,15 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: {
     '@tailwindcss/postcss': {
       tailwindConfig: './tailwind.config.js'
