19b232b22114ba6adb40803897c9da8d3faadf70
omkarkalagi <omkardigambar4@gmail.com>
2025-07-31 03:54:39 +0530

    Complete ESLint overhaul: Fixed tsconfig and simplified config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -17,7 +17,7 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: './tsconfig.eslint.json', // Point to our new config
+        project: './tsconfig.eslint.json',
         ecmaFeatures: {
           jsx: true,
         },
