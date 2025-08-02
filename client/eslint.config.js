dbb21cd455d3755e0379c7fde8a187d6d1a32cfc
omkarkalagi <omkardigambar4@gmail.com>
2025-08-02 13:54:00 +0530

    hjhhjh

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,14 +3,18 @@ import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import reactPlugin from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+import eslint from '@eslint/js';
 
 export default [
-  js.configs.recommended,
+  eslint.configs.recommended,
   {
     files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     plugins: {
       'react': reactPlugin,
-      '@typescript-eslint': tsPlugin
+      '@typescript-eslint': tsPlugin,
+      'react-refresh': reactRefresh
     },
     languageOptions: {
       parser: tsParser,
@@ -29,11 +33,17 @@ export default [
       },
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
       "no-unused-vars": "warn",
       "no-console": "off",
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     }
   },
   {
