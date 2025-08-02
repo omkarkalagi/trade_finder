2837837203a4cb2dfeb2942c056e82e2123f93a0
omkarkalagi <omkardigambar4@gmail.com>
2025-08-02 13:51:52 +0530

    bhhghg

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import axios from 'axios';
 
 const Register = () => {
